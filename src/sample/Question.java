5fbf75812cae70459f16402aa4f8240862485303
Silas <silaskeim02@gmail.com>
2020-04-21 11:40:25 +0200

    Hinzufügen der Klasse Question

---
+++ src/sample/Question.java
@@ -0,0 +1,25 @@
+package sample;
+
+//Die Klasse welche eine Frage und die dazugehörigen Antworten speichert
+public class Question {
+    private String question;
+    //Die Antwort an der Stelle answers[0] ist immer die korrekte antwort
+    private String[] answers = new String[3];
+    private int i = 0;
+
+    public Question(String question){
+        this.question = question;
+    }
+
+    public void addAnswer(String answer){
+        answers[i] = answer;
+        i++;
+    }
+
+    public String getAnswers(int i) {
+        return answers[i];
+    }
+    public String getQuestion() {
+        return question;
+    }
+}
