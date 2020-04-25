a3d6820fce696ded2e51b121b540ca75c5bc5277
Silas <silaskeim02@gmail.com>
2020-04-25 17:15:18 +0200

    Hinzufügen der Methode RandomizeQuestion, welche die Fragen und antworten zufällig auswählt

--- src/sample/Quizlayout.java
+++ src/sample/Quizlayout.java
@@ -4,6 +4,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Quizlayout {
 
         @FXML
@@ -18,8 +21,37 @@ public class Quizlayout {
         @FXML
         private TextField frage;
 
+        //ArrayList zum Speichern der bereits angezeigten Fragen
+        private ArrayList<Question> duplicate = new ArrayList<Question>();
 
+        public void randomizeQuestion (FileHandler fileHandler){
+                Random rand = new Random();
+                //Auswählen einer Zufälligen Frage
+                int rand_int1 = rand.nextInt(fileHandler.questions.length);
 
+                //Überprüfen ob die Frage bereits angezeigt wurde
+                for(int i = 0; i < duplicate.size(); i++){
+                        if(fileHandler.questions[rand_int1] == duplicate.get(i)){
+                                rand_int1 = rand.nextInt(fileHandler.questions.length);
+                                i = 0;
+                        }
+                }
+                //Frage anzeigen
+                frage.setText(fileHandler.questions[rand_int1].getQuestion());
+                //Frage zum duplicateArray hinzufügen
+                duplicate.add(fileHandler.questions[rand_int1]);
+
+                //Antworten werden zufällig zugeordnet
+                answer1.setText(fileHandler.questions[rand_int1].getAnswers(rand.nextInt(3)));
+                answer2.setText(fileHandler.questions[rand_int1].getAnswers(rand.nextInt(3)));
+                while(answer1.getText().equals(answer2.getText())){
+                        answer2.setText(fileHandler.questions[rand_int1].getAnswers(rand.nextInt(3)));
+                }
+                answer3.setText(fileHandler.questions[rand_int1].getAnswers(rand.nextInt(3)));
+                while (answer3.getText().equals(answer1.getText())||answer3.getText().equals(answer2.getText())){
+                        answer3.setText(fileHandler.questions[rand_int1].getAnswers(rand.nextInt(3)));
+                }
+        }
 }
 
 
